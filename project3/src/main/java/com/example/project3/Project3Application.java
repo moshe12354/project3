6795aa21c28da44263f9786097f848e159b2a8ae
takfi <takfir719@gmail.com>
2026-02-02 15:51:53 +0200

    commit with swagger

--- project3/src/main/java/com/example/project3/Project3Application.java
+++ project3/src/main/java/com/example/project3/Project3Application.java
@@ -1,9 +1,8 @@
 package com.example.project3;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.SpringApplication;
+
 public class Project3Application {
 
 	public static void main(String[] args) {
